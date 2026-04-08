8bebbc25edf0b6e1d1671527e9afece84183960e
Alexandr Titarenko <nook17n1@gmail.com>
2026-04-08 15:50:06 +0300

    Solution1

--- app/main.py
+++ app/main.py
@@ -1,22 +1,18 @@
 import os
 
 
-class FileNotFoundError(Exception):
-    pass
-
-
-def move_file(command: str) -> None:
+def move_file(command: str) -> str:
     parts = command.split()
     if len(parts) != 3 or parts[0] != "mv":
         return
-    file1, file2 = parts[1], parts[2]
+    _, file1, file2 = parts
     if file1 == file2:
         return
     try:
         if "/" not in file2:
             os.rename(file1, file2)
         elif "/" in file2:
-            new_path = "/".join(file2.split("/")[:-1]) + "/"
+            new_path = os.path.dirname(file2)
             os.makedirs(new_path, exist_ok=True)
             os.replace(file1, file2)
 
