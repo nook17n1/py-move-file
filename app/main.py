c6720bd7536011362bfc4b24bbaac0470a54f536
Alexandr Titarenko <nook17n1@gmail.com>
2026-04-09 09:45:33 +0300

    Solution3

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import os
 
 
-def move_file(command: str) -> str:
+def move_file(command: str) -> None:
     parts = command.split()
     if len(parts) != 3 or parts[0] != "mv":
         return
@@ -9,12 +9,10 @@ def move_file(command: str) -> str:
     if file1 == file2:
         return
     try:
-        if "/" not in file2:
-            os.rename(file1, file2)
-        elif "/" in file2:
-            new_path = os.path.dirname(file2)
-            os.makedirs(new_path, exist_ok=True)
-            os.replace(file1, file2)
+        directory_path = os.path.dirname(file2)
+        if directory_path:
+            os.makedirs(directory_path, exist_ok=True)
+        os.replace(file1, file2)
 
     except FileNotFoundError:
-        return "File not exist"
+        return
